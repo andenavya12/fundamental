7181b224d9fe18d5e4bde351684fc526f075bf56
Navy <Navy@Navya>
2019-06-27 22:34:42 +0530

    fundametal

---
+++ fundametal/src/fundametal/proj6.java
@@ -0,0 +1,14 @@
+package fundametal;
+
+public class proj6 {
+	public static void main(String[]args){
+		if(args.length==0)
+		System.out.println("No values");
+		else
+		{
+		for(String i:args)
+		System.out.print(i+",");
+		}
+		}
+
+}
