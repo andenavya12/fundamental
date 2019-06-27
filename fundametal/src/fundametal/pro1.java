e5f35c96b6656b49f49c338987cc9edf7d93d1ed
Navy <Navy@Navya>
2019-06-27 22:14:59 +0530

    program

--- fundametal/src/fundametal/pro1.java
+++ fundametal/src/fundametal/pro1.java
@@ -1,13 +1,10 @@
 package fundametal;
 
 public class pro1 {
-public static void main(String[]args){
-		int a=Integer.parseInt(args[0]);
-		if(a>0)
-		System.out.println("Positive");
-		else if(a<0)
-		System.out.println("Negative");
-		else 
-		System.out.println("Zero");
+	
+		public static void main(String args[]) {
+			String name=args[0];
+			String location=args[1];
+			System.out.println(name+ " technologies "  +location);
 		}
-		}
+	}
--- fundametal/src/fundametal/proj2.java
+++ fundametal/src/fundametal/proj2.java
@@ -1,5 +1,11 @@
 package fundametal;
 
 public class proj2 {
+	
 
+
+public static void main (String args[]) {
+	 
+	System.out.println("welcome "+args[0]);
+}
 }
