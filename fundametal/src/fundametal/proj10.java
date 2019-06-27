219a17110c5d512080f2e3776a249aa200b599b6
Navy <Navy@Navya>
2019-06-27 23:14:28 +0530

    fundametal

---
+++ fundametal/src/fundametal/proj10.java
@@ -0,0 +1,19 @@
+package fundametal;
+import java.util.*;
+
+public class proj10 {
+	public static void main(String args[]) {
+	      char ch;
+	       int temp;
+	       Scanner sc = new Scanner(System.in);
+	       System.out.print("Lowercase:alphabet: ");
+	       ch = sc.next().charAt(0);
+	temp = (int) ch;
+	       temp = temp - 32;
+	       ch = (char) temp;
+
+	       System.out.print("Upper case->" +ch);
+	   }
+}
+
+
